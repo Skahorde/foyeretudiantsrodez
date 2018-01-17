74ce8c28f05076866d3663724826f5fcfde8fd5a
Emmanuel Perez <contact@emmanuel-perez.com>
2018-01-17 15:53:49 +0100

    Page d'accueil v0.1

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- HTML -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="public/css/app.css"/>
+	</head>
+
+	<body>
+		<section id="home_section">			
+			<div id="home_background">
+				<div id="home_div">
+				
+					<p class="section-title">LE FOYER ST-PIERRE</p>
+
+					<hr class="separator">
+
+					<p class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+
+					<div class="center">
+						<button class="rounded-button" id="about_button">En savoir plus</button>
+					</div>
+
+				</div>
+			</div>
+		</section>
+	</body>
