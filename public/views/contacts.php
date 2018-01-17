32d16969e2a79189581f9d6926df85696baa3b0f
thomassayyer <thomas.sayer@outlook.fr>
2018-01-17 13:58:59 +0100

    Intégration de la page de contacts

---
+++ public/views/contacts.php
@@ -0,0 +1,9 @@
+<section id="contact_section">
+
+	<h1 class="section-title">CONTACTEZ-NOUS</h1>
+	<hr class="separator">
+	<h2 class="section-subtitle">
+		Il est possible de nous contacter par email, Facebook ou par téléphone pour toute demande de renseignement et/ou d'inscription.
+	</h2>
+
+</section>
