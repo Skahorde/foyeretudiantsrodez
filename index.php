6fb09a3cd964938f719eeccde062afbfb69752f2
thomassayyer <thomas.sayer@outlook.fr>
2018-01-17 16:10:15 +0100

    Essai container flex

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="public/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="public/css/app.css">
 </head>
 <body>
 
--- public/views/contacts.php
+++ public/views/contacts.php
@@ -5,5 +5,29 @@
 	<h2 class="section-subtitle">
 		Il est possible de nous contacter par email, Facebook ou par téléphone pour toute demande de renseignement et/ou d'inscription.
 	</h2>
+	<div class="container">
+		
+		<div>
+			
+			<i class="fa fa-phone"></i><br>
+			07.79.82.86.12
+
+		</div>
+
+		<div>
+			
+			<i class="fa fa-phone"></i><br>
+			07.79.82.86.12
+
+		</div>
+
+		<div>
+			
+			<i class="fa fa-phone"></i><br>
+			07.79.82.86.12
+
+		</div>
+
+	</div>
 
 </section>
