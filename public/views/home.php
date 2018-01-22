b58da21df3d25d45354e1442b627ca8e0673efc5
Emmanuel Perez <contact@emmanuel-perez.com>
2018-01-22 09:44:24 +0100

    HomePage - Version Finale

--- public/views/home.php
+++ public/views/home.php
@@ -8,7 +8,7 @@
 
 			<p class="section-subtitle">Bienvenue sur le site du foyer St-Pierre de Rodez.</p>
 
-			<div class="block-center">
+			<div class="block-center" id="about_button_container">
 				<button class="rounded-button" id="about_button">En savoir plus</button>
 			</div>
 
