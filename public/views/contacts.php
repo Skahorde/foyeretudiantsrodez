c8729c9afcdc94ee93c54064021250369dd22f92
thomassayyer <thomas.sayer@outlook.fr>
2018-01-22 09:10:58 +0100

    Fin développement section de contact

--- public/views/contacts.php
+++ public/views/contacts.php
@@ -5,27 +5,27 @@
 	<h2 class="section-subtitle">
 		Il est possible de nous contacter par email, Facebook ou par téléphone pour toute demande de renseignement et/ou d'inscription.
 	</h2>
-	<div class="container">
+	<div class="container block-center">
 		
-		<div>
-			
-			<i class="fa fa-phone"></i><br>
-			07.79.82.86.12
-
+		<div class="contact-link">
+			<a href="tel:+33565688282">
+				<i class="fa fa-2x fa-phone"></i><br>
+				+33 5 65 68 82 82
+			</a>
 		</div>
 
-		<div>
-			
-			<i class="fa fa-phone"></i><br>
-			07.79.82.86.12
-
+		<div class="contact-link">
+			<a href="https://www.facebook.com/foyer.saintpierre">
+				<i class="fa fa-2x fa-facebook-official"></i><br>
+				Foyer Etudiants Saint Pierre
+			</a>
 		</div>
 
-		<div>
-			
-			<i class="fa fa-phone"></i><br>
-			07.79.82.86.12
-
+		<div class="contact-link">
+			<a href="mailto:foyeretudiants@gmail.com">
+				<i class="fa fa-2x fa-envelope-o"></i><br>
+				foyeretudiants@gmail.com
+			</a>
 		</div>
 
 	</div>
