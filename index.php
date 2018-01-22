c0fa479ec444b6ed981ad23bfc624f9726f65d2b
Emmanuel Perez <contact@emmanuel-perez.com>
2018-01-22 09:38:58 +0100

    HomePage - Version finale non fonctionelle

--- index.php
+++ index.php
@@ -6,21 +6,6 @@
 	</head>
 
 	<body>
-		<section id="home_section">			
-			<div id="home_background">
-				<div id="home_div">
-				
-					<p class="section-title">LE FOYER ST-PIERRE</p>
-
-					<hr class="separator">
-
-					<p class="section-subtitle">Bienvenue sur le site du foyer St-Pierre de Rodez.</p>
-
-					<div class="center">
-						<button class="rounded-button" id="about_button">En savoir plus</button>
-					</div>
-
-				</div>
-			</div>
-		</section>
+		<?php include 'public/views/home.php' ?>
 	</body>
+</html>
