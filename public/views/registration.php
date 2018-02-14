9b012e574e830da6d9b462306d8e998609829302
Emmanuel Perez <contact@emmanuel-perez.com>
2018-02-14 11:37:09 +0100

    v1

--- public/views/registration.php
+++ public/views/registration.php
@@ -1,35 +1,38 @@
-<section id="student_home_section">
+<section id="registration_section">
 
-	<h1 class="section-title" id="title_foyer">INSCRIVEZ-VOUS DÈS MAINTENANT</h1>
-	<hr class="separator" id="separator_foyer">
+	<h1 class="section-title" id="title_registration">INSCRIVEZ-VOUS DÈS MAINTENANT</h1>
+	<hr class="separator" id="separator_registration">
 	
-	<h2 class="section-subtitle" id="subtitle_foyer">
-		Le Foyer Étudiant St-Pierre est situé dans le quartier de Bourran, aux portes de Rodez.<br>
-		Il bénéficie d'un cadre spatieux et privilégié, offrant aux étudiants une véritable expérience de cohésion de groupe.
+	<h2 class="section-subtitle" id="subtitle_registration">
+		Vous voulez nous rejoindre ? Rien de plus simple ! Il vous suffit de remplir un formulaire de pré-inscription.<br>
+		Celui-ci est disponible ci-dessous sous différents formats. Merci de nous le retourner une fois complété.<br>
+		(cf. <a class="page-scroll link-registration" href="#contacts_section">contact - accès</a>)
 	</h2>
 
-	<div class="block-center container" id="infoboxes">
+	<div class="block-center container" id="docs">
 		
-		<div class="infobox">
+		<div class="doc">
 				<li>
-					<i class="circle-icon fa-3x fa fa-bed"></i>
-					<p class="p-icon">54 places</p>
+					<a href="storage/Fiche de pre-inscription.pdf">
+						<i class="circle-icon fa-3x fa fa-file-pdf-o"></i><br>
+						.pdf
+					</a>
 				</li>
 		</div>
-		<div class="infobox">
+		<div class="doc">
 				<li>
-					<div>
-						<i class="circle-icon fa-3x fa fa-calendar-o"></i>
-						<p class="p-icon">1er septembre</p>
-						<i class="arrow-icon fa fa-arrow-down"></i>
-						<p class="p-icon">30 juin</p>
-					</div>
+					<a href="storage/Fiche de pre-inscription.doc">
+						<i class="circle-icon fa-3x fa fa-file-text-o"></i><br>
+						.doc
+					</a>				
 				</li>
 		</div>
-		<div class="infobox">
+		<div class="doc">
 				<li>
-					<i class="circle-icon fa-3x fa fa-bus"></i>
-					<p class="p-icon"><a class="page-scroll" href="#contacts_section">À 5 minutes de tout</a></p>
+					<a href="storage/Fiche de pre-inscription.odt">
+						<i class="circle-icon fa-3x fa fa-file-o"></i><br>
+						.odt
+					</a>				
 				</li>
 		</div>		
 	</div>
