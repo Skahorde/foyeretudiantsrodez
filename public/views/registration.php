76ea0c13b73efba012d244501a12b6806d08f8aa
Emmanuel Perez <contact@emmanuel-perez.com>
2018-02-14 10:43:36 +0100

    v0.2

---
+++ public/views/registration.php
@@ -0,0 +1,37 @@
+<section id="student_home_section">
+
+	<h1 class="section-title" id="title_foyer">INSCRIVEZ-VOUS DÈS MAINTENANT</h1>
+	<hr class="separator" id="separator_foyer">
+	
+	<h2 class="section-subtitle" id="subtitle_foyer">
+		Le Foyer Étudiant St-Pierre est situé dans le quartier de Bourran, aux portes de Rodez.<br>
+		Il bénéficie d'un cadre spatieux et privilégié, offrant aux étudiants une véritable expérience de cohésion de groupe.
+	</h2>
+
+	<div class="block-center container" id="infoboxes">
+		
+		<div class="infobox">
+				<li>
+					<i class="circle-icon fa-3x fa fa-bed"></i>
+					<p class="p-icon">54 places</p>
+				</li>
+		</div>
+		<div class="infobox">
+				<li>
+					<div>
+						<i class="circle-icon fa-3x fa fa-calendar-o"></i>
+						<p class="p-icon">1er septembre</p>
+						<i class="arrow-icon fa fa-arrow-down"></i>
+						<p class="p-icon">30 juin</p>
+					</div>
+				</li>
+		</div>
+		<div class="infobox">
+				<li>
+					<i class="circle-icon fa-3x fa fa-bus"></i>
+					<p class="p-icon"><a class="page-scroll" href="#contacts_section">À 5 minutes de tout</a></p>
+				</li>
+		</div>		
+	</div>
+
+</section>
