6aad418a5cde1f3af3c0f04f8097cea7e40b20d6
Emmanuel Perez <contact@emmanuel-perez.com>
2018-03-01 17:36:42 +0100

    Version figée

---
+++ public/views/feedbacks.php
@@ -0,0 +1,24 @@
+<section id="feedbacks_section">
+
+	<h2 class="section-title" id="title_feedbacks">L'AVIS DES ÉTUDIANTS</h1>
+	<hr class="separator" id="separator_feedbacks">
+	
+	<p class="section-subtitle" id="subtitle_feedbacks">
+		Retrouvez les avis laissés sur notre Livre d'Or par d'anciens ou d'actuels étudiants logés au Foyer.<br>
+	</p>
+
+	<div class="slideshow" id="feedbacks">
+		<ul>
+			<li class="feedback">				
+				<p class="content"> La vie au foyer St Pierre : des moments conviviaux de partage, l'occasion de faire de magnifiques rencontres !</p>
+				<p class="name">- Manon</p>
+			</li>
+			<br>
+			<li class="feedback">				
+				<p class="content">Le foyer St Pierre c'est génial! La vie est belle, l'ambiance est super, on fait des rencontres magiques, on passe de super moments. Quand on y est on ne veut pas repartir! Je le conseille à tous!</p>
+				<p class="name">- Coralie</p>
+			</li>
+		</ul>
+	</div>
+
+</section>
